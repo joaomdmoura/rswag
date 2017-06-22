de3c4532c631432340870d8586ad6530772d64ed
João M. D. Moura <joaomdmoura@gmail.com>
2017-06-22 10:06:19 -0300

    adding form support

--- test-app/db/schema.rb
+++ test-app/db/schema.rb
@@ -11,12 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160218212104) do
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "title", limit: 255
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "blogs", :force => true do |t|
+    t.string   "title", limit: 255
+    t.text     "content"
+    t.string   "thumbnail"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
-
 end
